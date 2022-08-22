87e676ffcf6ed95fcd1c79cdeb315c7591394846
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2022-08-22 23:55:10 +0700

    wire up puppeteer

---
+++ functions/screenshot.ts
@@ -0,0 +1,48 @@
+import type { Handler } from '@netlify/functions'
+import type { Browser } from 'puppeteer-core'
+import { builder } from '@netlify/functions'
+import chrome from '@sparticuz/chrome-aws-lambda'
+
+const getBrowser = async () => chrome.puppeteer.launch({
+    args: chrome.args,
+    headless: true,
+    ignoreHTTPSErrors: true,
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath
+})
+
+const getImage:Handler = async () => {
+
+    let browser: Browser|null = null
+    let image: string|Buffer|null = null
+
+    try {
+        browser = await getBrowser()
+        const page = await browser.newPage()
+        await page.goto('https://sngr.studio', { waitUntil: ['networkidle0'] })
+        image = await page.screenshot()
+    } catch(e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        }
+    } finally {
+        if (browser !== null) {
+            await browser.close()
+        }
+    }
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'image/jpeg',
+            'Content-Length': image.length
+        },
+        body: image.toString('base64'),
+        isBase64Encoded: true
+    }
+}
+
+const handler = builder(getImage)
+
+export { handler }

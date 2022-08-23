81991dadc6439ece4e4fa5d81ab4725c8d4be1cd
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2022-08-23 12:20:23 +0700

    downgrade chrome package

--- functions/screenshot.ts
+++ functions/screenshot.ts
@@ -1,7 +1,7 @@
 import type { Handler } from '@netlify/functions'
 import type { Browser } from 'puppeteer-core'
 import { builder } from '@netlify/functions'
-import chrome from '@sparticuz/chrome-aws-lambda'
+import chrome from 'chrome-aws-lambda'
 
 const getBrowser = async () => chrome.puppeteer.launch({
     args: chrome.args,
@@ -20,7 +20,7 @@ const getImage:Handler = async () => {
         browser = await getBrowser()
         const page = await browser.newPage()
         await page.goto('https://sngr.studio', { waitUntil: ['networkidle0'] })
-        image = await page.screenshot()
+        image = await page.screenshot() as Buffer
     } catch(e) {
         return {
             statusCode: 500,

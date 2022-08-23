241d4680ce3fb430ca57d7c91a5d58c2d0f124e3
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2022-08-23 15:35:34 +0700

    implement parameters

--- functions/screenshot.ts
+++ functions/screenshot.ts
@@ -3,6 +3,15 @@ import type { Browser } from 'puppeteer-core'
 import { builder } from '@netlify/functions'
 import chrome from 'chrome-aws-lambda'
 
+const isURL = (url:string) => {
+    try {
+        new URL(url)
+        return true
+    } catch (e) {
+        return false
+    }
+}
+
 const getBrowser = async () => chrome.puppeteer.launch({
     args: chrome.args,
     headless: true,
@@ -11,7 +20,14 @@ const getBrowser = async () => chrome.puppeteer.launch({
     executablePath: await chrome.executablePath
 })
 
-const getImage:Handler = async () => {
+const getImage:Handler = async (event) => {
+    const params = event.path.split('/')
+    if (!isURL(decodeURIComponent(params.at(-1)))) throw new Error(`Please provide decoded URL, e.g: '/image/https%3A%2F%2Fwww.sngr.studio'`)
+    const url = new URL(decodeURIComponent(params.at(-1)))
+    const options = {
+        width: Number(params.find(p => p.endsWith('w'))?.replace('w', '')) || 1280,
+        height: Number(params.find(p => p.endsWith('h'))?.replace('h', '')) || 720
+    }
 
     let browser: Browser|null = null
     let image: string|Buffer|null = null
@@ -19,8 +35,12 @@ const getImage:Handler = async () => {
     try {
         browser = await getBrowser()
         const page = await browser.newPage()
-        await page.goto('https://sngr.studio', { waitUntil: ['networkidle0'] })
-        image = await page.screenshot() as Buffer
+        await page.setViewport({
+            width: options.width,
+            height: options.height
+        })
+        await page.goto(url.href, { waitUntil: ['networkidle2'] })
+        image = await page.screenshot({ type: 'jpeg', quality: 80 }) as Buffer
     } catch(e) {
         console.error(e.message)
         return {
@@ -40,7 +60,8 @@ const getImage:Handler = async () => {
             'Content-Length': image.length
         },
         body: image.toString('base64'),
-        isBase64Encoded: true
+        isBase64Encoded: true,
+        ttl: 60 * 60 * 24 * 30 * 3
     }
 }
 
